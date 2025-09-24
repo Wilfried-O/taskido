ea79982500d4871cc491ce51993a982692d0ce76
Wilfried Okou <will.okson@gmail.com>
2025-09-24 19:31:40 -0400

    Add components to display the list of todos
    Each Todo can be marked as either completed or active.
    Also, each Todo can be removed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
     const [todos, setTodos] = useState([]);
@@ -8,13 +9,26 @@ function App() {
     const addTodo = (todo) => {
         setTodos([...todos, todo]);
     };
-    // to view *todos*, you can print to the console: uncomment next line
-    // console.log(todos);
+    
+    const toggleTodo = (id) => {
+        setTodos(
+            todos.map( todo =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
+
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
 
     return (
         <div className="app-container">
             <h1>Taskido</h1>
             <TodoForm addTodo={addTodo} />
+            <TodoList 
+                todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}
+            />
         </div>
     );
 }
